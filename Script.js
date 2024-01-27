0772ef225e6019a0295e5af9426b1e4820d05243
Rakavi Radhakrishnan <153973093+RakaviRadhakrishnan@users.noreply.github.com>
2024-01-27 11:00:39 +0530

    Script.js

---
+++ Script.js
@@ -0,0 +1,48 @@
+let Resume={
+    "basics": {
+      "name": "RAKAVI R",
+      "email": "rakaviradh@gmail.com",
+      "phone": 987654321,
+      "degree": "MCA",
+      "location": {
+        "address": "1,athisamy nagar",
+        "postalCode": 600001,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+          "work": [
+      {
+        "company": "TCS",
+        "position": "System Engineer",
+        "startDate": "2019-09-02",
+        "endDate": "TILL",
+      },
+    ],
+    "education": [
+      {
+        "institution": "PMIST",
+        "department": "CSC",
+        "studyType": "fulltime",
+        "batch start year": 2016,
+        "batch end year": 2019,
+        "gpa": 6.5,
+    ],
+    "skills": [
+      {
+        "name": "HTML,PHP",
+        "level": "beginer",
+        "project": [
+          "MOBILE APP"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+
+  }
+  console.log(Resume);
+  
